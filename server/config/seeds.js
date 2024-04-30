20d9c3b910c695c53eed7ec3b1e1bc024d9a9f91
Marius Du Plooy <woestyn101@gmail.com>
2024-04-30 10:55:06 -0400

    seeds updated

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -4,17 +4,30 @@ const cleanDB = require('./cleanDB');
 const foodData = require('./food.json');
 const usersData = require('./users.json');
 db.once('open', async () => {
-  await cleanDB('Food', 'foods');
-  await cleanDB('User', 'users');
 
+  try{
+    await cleanDB('Food', 'foods');
+    await cleanDB('User', 'users');
 
-  await Food.insertMany(foodData);
+    await User.create(usersData);
+  
+  
+    for (let i = 0; i < foodData.length; i++) {
+      const { _id, foodAuthor } = await Food.create(foodData[i]);
+      const user = await User.findOneAndUpdate(
+        { username: foodAuthor },
+        {
+          $addToSet: {
+            foods: _id,
+          },
+        }
+      );
+    }
 
-  console.log('Recipes seeded');
-
-  await User.insertMany(usersData);
-
-  console.log('users seeded');
-
-  process.exit();
+  }catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('all done!');
+  process.exit(0);
 });
