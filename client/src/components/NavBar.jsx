d22aeb8d572e962b18bc662399471bcd6a91ac89
zachary chapman <zach.chapman24@gmail.com>
2024-05-01 18:45:06 -0400

    worked on nav bar to show right info based on logged in or out

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,67 +1,74 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../components/styles/navBar.css';
-import Auth from '../utils/auth'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../components/styles/navBar.css";
+import Auth from "../utils/auth";
+
+import { useState } from "react";
+import auth from "../utils/auth";
 
 const NavBar = () => {
+  const [loggedIn, setLoggedIn] = useState(auth.loggedIn());
+
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
+    setLoggedIn(false);
   };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-orange">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Home</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto"> 
-          <li className="nav-item">
-              <Link className="nav-link" to="/dashboard">Dashboard</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/signup">SignUp</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-            <li className="nav-item">
-            <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </li>
-
-             {/* {Auth.loggedIn() ? (
-              <>
+          <ul className="navbar-nav ms-auto">
+            {loggedIn && (
               <li className="nav-item">
-              <Link className="nav-link" to="/dashboard">Dashboard</Link>
-            </li>
-            <li className="nav-item">
-            <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </li>
-              </>
-
-             ) : (
-                <>
+                <Link className="nav-link" to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
+            )}
+            {loggedIn ? (
+              <li className="nav-item">
+                <button
+                  className="nav-link btn btn-lg btn-light m-2"
+                  onClick={logout}
+                >
+                  Logout
+                </button>
+              </li>
+            ) : (
+              <React.Fragment>
                 <li className="nav-item">
-              <Link className="nav-link" to="/signup">SignUp</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-                
-                </>
+                  <Link className="nav-link" to="/signup">
+                    Sign Up
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              </React.Fragment>
+            )}
 
-             )} */}
-            
-            
-            
-            
             <li className="nav-item">
-              <Link className="nav-link" to="/chicken">Chicken</Link>
+              <Link className="nav-link" to="/chicken">
+                Chicken
+              </Link>
             </li>
           </ul>
         </div>
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -4,18 +4,21 @@ class AuthService {
     return decode(this.getToken());
   }
   loggedIn() {
+    console.log("test1");
     const token = this.getToken();
     return token && !this.isTokenExpired(token) ? true : false;
   }
   isTokenExpired(token) {
+    console.log("test2");
     const decoded = decode(token);
-    if (decoded.exp < Date.now()) {
-      localStorage.removeItem("id_token");
-      return true;
-    }
+    // if (decoded.exp < Date.now()) {
+    //   localStorage.removeItem("id_token");
+    //   return true;
+    // }
     return false;
   }
   getToken() {
+    console.log("test3");
     return localStorage.getItem("id_token");
   }
   login(idToken) {
