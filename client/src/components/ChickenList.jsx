67044ba0438aba289d6250d213347152cc195cc5
zachary chapman <zach.chapman24@gmail.com>
2024-05-02 19:37:17 -0400

    tets

--- client/src/components/ChickenList.jsx
+++ client/src/components/ChickenList.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const FoodCard = ({ food }) => {
   return (
     <div className="col mb-4">
       <div className="card h-100 w-100">
-        <div className="card-header bg-dark text-light">
-          {food.name}
-        </div>
+        <div className="card-header bg-dark text-light">{food.name}</div>
         <div className="card-body">
-          <h6><strong><u>Description</u></strong></h6>
+          <h6>
+            <strong>
+              <u>Description</u>
+            </strong>
+          </h6>
           <p className="card-text">{food.description}</p>
-          <h6><strong><u>Instructions</u></strong></h6>
+          <h6>
+            <strong>
+              <u>Instructions</u>
+            </strong>
+          </h6>
           <p className="card-text">{food.instructions}</p>
-          <h6><strong><u>Ingredients</u></strong></h6>
+          <h6>
+            <strong>
+              <u>Ingredients</u>
+            </strong>
+          </h6>
           <p className="card-text">{food.ingredients}</p>
-          <h6><strong><u>Image</u></strong></h6>
+          <h6>
+            <strong>
+              <u>Image</u>
+            </strong>
+          </h6>
           <p className="card-text">{food.image}</p>
         </div>
       </div>
@@ -28,7 +42,7 @@ const FoodList = ({ foods }) => {
   }
 
   return (
-  <div className="container-fluid">
+    <div className="container-fluid">
       <div className="row">
         {foods.map((food, i) => (
           <FoodCard key={i} food={food} />
