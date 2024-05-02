dc709203b66443c254a5d4ff53c469c0a838b530
Ellie22302 <146311968+Ellie22302@users.noreply.github.com>
2024-05-02 19:29:26 -0400

    added image display

--- client/src/components/UserFoodList.jsx
+++ client/src/components/UserFoodList.jsx
@@ -46,14 +46,15 @@ const Profile = () => {
             foods={user.foods}
            
           />
-        </div>
+          <img src={user.image}></img>
+              </div>
         {!userParam && (
           <div
             className="col-12 col-md-10 mb-3 p-3"
             style={{ border: '1px dotted #1a1a1a' }}
           >
            
-          </div>
+            </div>
         )}
       </div>
     </div>
