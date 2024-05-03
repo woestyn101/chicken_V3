deb796a78378bfbafa0d473cf7dcc35d8140be84
Marius Du Plooy <woestyn101@gmail.com>
2024-05-03 19:56:03 -0400

    fix authenication is not a constructor

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../components/styles/navBar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../components/styles/navBar.css';
+import { useQuery } from '@apollo/client';
+import ChickenList from './ChickenList';
+import { QUERY_FOODS } from '../utils/queries';
 import RecipeForm from "./AddFood";
+import FoodList2 from "./ChickenList2";
+import UserFoodList from './UserFoodList'
 
-const Dashboard = () => {
+const Chicken = () => {
+  const { loading, data } = useQuery(QUERY_FOODS);
+  const foods = data?.foods || [];
   return (
     <>
-      <RecipeForm></RecipeForm>
+    <h1>Dashboard</h1>
+    <RecipeForm></RecipeForm>
+    <h2>Your recipes:</h2>
+    <UserFoodList></UserFoodList>
+    <ChickenList 
+     foods={foods}/>
+      <ChickenList image={foods.image}/>
+
+    
+    
     </>
+   
   );
 };
 
-export default Dashboard;
+export default Chicken;
---
+++ client/src/components/UpdateFood.jsx
@@ -0,0 +1,135 @@
+import { useState } from 'react';
+// Important for useMutation: We import the useMutation hook from @apollo/client
+import { useMutation } from '@apollo/client';
+
+// Important for useMutation: We import the specific query we'd like to perform from the mutations.js utility
+import { ADD_FOOD } from '../utils/mutations';
+
+const RecipeForm = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [image, setImage] = useState('');
+  const [foodAuthor, setfoodAuthor] = useState('');
+  
+
+  // Important for useMutation: We pass the mutation we'd like to execute to the useMutation hook
+  // The useMutation hook returns an array. The function at index 0 can be dispatched within the component to trigger the mutation query
+  // The object at index 1 contains information, such as the error boolean, which we use in this application
+  const [addFood, { error }] = useMutation(ADD_FOOD);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Important for useMutation: Here we want the mutation to occur in response to a form submission
+      // The mutation we want to run also requires mutation parameters to be passed, which we deliver as a variables object
+      // let foodData = {
+      //    name: name,
+      //    description: description,
+      //    instructions: instructions,
+      //    ingredients: ingredients,
+      //    image: image
+
+      // }
+      const { data } = await addFood({
+        variables: { 
+          name,
+          description,
+          instructions,
+          ingredients,
+          image,
+          foodAuthor
+
+        },
+       
+        
+      });
+
+      window.location.reload();
+    } catch (err) {
+      console.error(err, "error here");
+    }
+  };
+
+  return (
+    <div className='container mt-4 d-flex justify-content-center align-items-center min-vh-100'>
+      <div className='card'>
+        <div className='card-header bg-dark text-light'>
+        Add your recipe:
+        </div>
+        <div className='card-body'>
+            <form
+        className="d-flex flex-column align-items-center "
+        onSubmit={handleFormSubmit}
+      >
+        <div className="col-12 col-lg-9">
+          <label>Name:
+          <input
+            placeholder="Recipe Name..."
+            value={name}
+            className="form-control"
+            onChange={(event) => setName(event.target.value)}
+          />
+          </label><br/>
+          <label>Description:
+          <input
+            placeholder="Description..."
+            value={description}
+            className="form-control"
+            onChange={(event) => setDescription(event.target.value)}
+          />
+          </label><br/>
+          <label>Instructions:
+          <input
+            placeholder="Instructions..."
+            value={instructions}
+            className="form-control"
+            onChange={(event) => setInstructions(event.target.value)}
+          />
+          </label><br/>
+          <label>Ingredients:
+          <input
+            placeholder="ingredients..."
+            value={ingredients}
+            className="form-control"
+            onChange={(event) => setIngredients(event.target.value)}
+          />
+          </label><br/>
+          <label>Image:
+          <input
+            placeholder="image..."
+            value={image}
+            className="form-control"
+            onChange={(event) => setImage(event.target.value)}
+          />
+          </label><br/>
+          <label>Author:
+          <input
+            placeholder="Author..."
+            value={foodAuthor}
+            className="form-control"
+            onChange={(event) => setfoodAuthor(event.target.value)}
+          />
+          </label><br/><br/>
+          <button className="btn btn-primary" type="submit">
+            Add Recipe
+          </button>
+        </div>      
+         
+       
+        {error && (
+          <div className="col-12 my-3 bg-danger text-white p-3">
+            Something went wrong...
+          </div>
+        )}
+      </form>
+      </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default RecipeForm;
